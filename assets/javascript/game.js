7f70aaa2f375f29e289172499003e1694633bd27
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-16 17:57:57 -0400

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,94 @@
 //what are my parts - vars - global b/c outside of a function
-var wordList = ["Superwomen", "Supergirl", "Harley Quinn", "Lois Lane", 
-    "Power Girl", "Captain Marvel", "Catwoman", "Black Widow", 
-    "She-Hulk", "Elektra", "Mystique", "Hela", "Nebula", 
-    "The Femme Fatales", "Gamora", "Green Lantern", "Invisible Woman", "Thor"];
+var wordList = ["Superwomen", "Supergirl", "Harley Quinn", "Lois Lane",
+    "Power Girl", "Captain Marvel", "Catwoman", "Black Widow",
+    "She-Hulk", "Elektra", "Mystique", "Hela", "Nebula",
+    "The Femme Fatales", "Gamora", "Green Lantern", "Invisible Woman", "Thor"
+];
+var blankSpaces = "_"
 var currentWord; //what word was random picked
 var lettersInWord; //what letters are in the current word
-var numSpots = 0; //blank for the current word
 var userGuess = []; //what letters the user has guessed
-var blankCorrect = [];  //correct letters go here
-var wrongLetters =[0];   //wrong letters
+var blankCorrect = []; //correct letters go here
+var wrongLetters = []; //wrong letters
 var wins = 0 //how many wins the player has
 var losses = 0 //how many losses the player has
 var guessesLeft = 10 //how many guesses the player has left
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //alphabet choices ?? not sure if I need this or not...
-var remainingLetters = letters - userGuess //what letters have not been picked
+var remainingLetters = userGuess //what letters have not been picked 
+var alphabetArray = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 
-//start game - any key selected
-//function startGame() {
-//    onkeydown
-//}
+//if game has not started, when a user has entered a letter, random word is picked and game starts
 
-document.addEventListener("keydown", startGame, false);
+//if current word is null start a game & pick a work, otherwise check guesses & only in that condition
 
-function startGame(e) {
-  var keyCode = e.keyCode;
-  
-  if(keyCode) {  
-    currentWord = wordList[Math.floor(Math.random() * wordList.length)]; //picks a random word from wordList
-    lettersInWord = currentWord.split(""); //.split = split a string into substring - shows how many letters are in the currentWord
-    numSpots = lettersInWord.length; //says how many blanks to make
-    guessesLeft = 10;  //how many guesses at start
-    wrongLetters = []; //how many wrong letters at start
-   
-    for (var i=0; i < numSpots; i++) { //what goes in the blanks
-        blankCorrect.push("_");
-
+currentWord = wordList[Math.floor(Math.random() * wordList.length)]; //picks a random word from wordList
+lettersInWord = currentWord.split(""); //.split = split a string into substring - shows how many letters are in the currentWord
+blankSpaces = lettersInWord.length; //says how many blanks to make
+for (var i = 0; i < lettersInWord; i++) { //loop to determine how many letters
+    blankSpaces.push("_"); //creates blanks
 }
+guessesLeft = 10; //how many guesses at start
+wrongLetters = []; //how many wrong letters
 
-console.log(numSpots)
+
+
+console.log(blankSpaces)
 console.log(currentWord)
 console.log(lettersInWord)
 console.log(blankCorrect)
-  } else {
-    alert("Oh no you didn't.");
-  }
-}
 
-
-//capture uer's guess
-document.onkeypress = function(event) {
-    var userGuess = event.key;
-
-    if (userGuess = lettersInWord) {
-        userGuess.push(lettersInWord); //push to user guess array
-    } else {    
-        guessesLeft -= 1; 
-        
+//guesses  - on event keyup checking guesses with event.key
+function checkGuesses(letter) { //you can name function anything you want
+    var lettersInWord = false; //see if letter is in the current word
+    for (var i = 0; i < blankSpaces; i++) { //if currentWord = letter entered, then :)
+        if (currentWord[i] == letter) {
+            lettersInWord = true;
+            guessesLeft--; //guess has been used
+        }
+    }
+    if (lettersInWord) { //does guessed letter match word?
+        for (var i = 0; i < blankSpaces; i++) {
+            if (currentWord[i] == letter) {
+                blankCorrect[i] = letter;
+            }
+        }
+console.log(blankCorrect);
+    } else { //if wrong letter guesses, then :(
+        wrongLetters.push(wrongletters); //put wrong letter in space wehre it should go ???
+        guessesLeft--; //guess has been used
     }
 }
+console.log(wrongLetters);
 
 
-//function -- to guess letter
-// should iterate through the word letters - 
-// & to see if it matches a letter in the word - index of letter
-// & log the letter guessed
-// & display the letter guessed in the correct blank
-// & figure out if there are more letters to guess
+///get results in page! :)
 
-document.onkeypress = function(event) {
-    var userGuess = event.key; 
+//startover if player wins or looses
+//function reset() {
+    //guessesLeft = 10;
+    //wrongLetters = [];
+    //blankSpaces = [];
+    //startGame();
+//}
 
-    if(userGuess ===  lettersInWord) //letter in the randomWord)
- 
+//start game - any key selected
 
-// var - remaining letters
-    var remainingLetters = word.length;
+//document.onkeyup = function (event) {
+    //console.log(event.key);
+//}
 
-
-
-
+function initializeGame () {
+    currentWord = "";
+    guessesLeft = 10;
+    wrongLetters = [];
+    blankSpaces = [];
+    
+    $("#currentWord, #guessesLeft, #wrongLetters, #blankSpaces").empty();
+  }
 
 //bonus* play a sound when the user guesses the word correctly
 
 
-//bonsu* stylish CSS rules to make design that fits game theme
+//bonus* stylish CSS rules to make design that fits game theme
 
-}
 
-//organize game code as object, except for key events that get to the letter guessed
+//bonus* organize game code as object, except for key events that get to the letter guessed
