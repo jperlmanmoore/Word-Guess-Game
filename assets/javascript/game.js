19e676685ec8b9ac1728d2da66eabc5483ddbdda
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-17 16:54:01 -0400

    getting close to finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,13 @@ var wordList = ["Superwomen", "Supergirl", "Harley Quinn", "Lois Lane",
     "The Femme Fatales", "Gamora", "Green Lantern", "Invisible Woman", "Thor"
 ];
 var currentWord = "";
-
-//if game has not started, when a user has entered a letter, random word is picked and game starts
-//how many at start
 var guessesLeft = 10;
 var gameMode = "not-started"; // not-started, started, ended 
-// guesses need to get lower after each guess
-//document.getElementById("guessesLeft"); - display how many guesess left
-
+var correctLetter = [];
+var wrongLetter = [];
 var blankSpaces = [];
 
+//make underscore placeholders for letters in current word
 function generateUnderscores() {
     for (i = 0; i < currentWord.length; i++) {
         blankSpaces.push("_");
@@ -21,44 +18,51 @@ function generateUnderscores() {
     return blankSpaces;
 }
 
-var correctLetter = [];
-var wrongLetter = [];
-
 //determine a guess
 document.addEventListener("keypress", (event) => {
     var letter = String.fromCharCode(event.keyCode).toLowerCase();
     console.log("letter = '" + letter + "'");
 
-    if(gameMode === "not-started") {
+    //at start of game
+    //when game starts *unfortunately the blank tiles don't show until a correct letter is fixed, with more time I would correct this.
+    //*also the blank space in two word letters counts as a guess, whcih I could correct.
+    if (gameMode === "not-started") {
         gameMode = "started";
-        guessesLeft = 10;
+        guessesLeft = 12;
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
         currentWord = wordList[Math.floor(Math.random() * wordList.length)];
         console.log(currentWord);
         blankSpaces = [];
+        document.getElementById("blankSpaces").innerHTML = blankSpaces.join("");
+        wrongLetter = [];
         console.log(generateUnderscores());
-        document.getElementById("blankSpaces").innerHTML = blankSpaces;
-    }
-    else if(gameMode === "started") {
+        document.getElementById("presstostart").style.display = "none";
+        document.getElementById("winorlose").innerHTML = "";
+        document.getElementById("wrongLetter").innerHTML = "";
+
+    
+    } else if (gameMode === "started") {
         //put correct letter is guessed 
         if (currentWord.toLowerCase().indexOf(letter) > -1) {
             //put in correct letter array
             correctLetter.push(letter);
             console.log("correctLetter:");
             console.log(correctLetter);
-        
+
             //replace underscore
             //blankSpaces[currentWord.indexOf(letter)] = letter;
             fillInLetters(blankSpaces, currentWord, letter);
             console.log("blankSpaces:");
             console.log(blankSpaces);
-            document.getElementById("blankSpaces").innerHTML = blankSpaces;
+            document.getElementById("blankSpaces").innerHTML = blankSpaces.join("");
 
-            if(blankSpaces.join("") === currentWord) {
+            if (blankSpaces.join("") === currentWord) {
                 //display if win or lose
                 document.getElementById("winorlose").innerHTML = "You won!";
-                gameMode = "ended";
+                document.getElementById("presstostart").style.display = "";
+                gameMode = "not-started";
             }
-        
+
         } else {
             //put wrong letter in array
             wrongLetter.push(letter);
@@ -66,37 +70,36 @@ document.addEventListener("keypress", (event) => {
             console.log(wrongLetter);
         }
 
+        //how may guesses left
         guessesLeft--;
         document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-        if(guessesLeft <= 0) {
+        //win or lose & show press to start again
+        if (guessesLeft <= 0) {
             document.getElementById("winorlose").innerHTML = "You Lost!";
-            gameMode = "ended";
+            document.getElementById("presstostart").style.display = ""
+            gameMode = "not-started";
         }
     }
-    else if(gameMode === "ended") {
 
-    }
 });
 
 //if run out of guesses, then you lose
 
 function fillInLetters(blankSpaces, currentWord, letter) {
-    for(var i = 0; i < blankSpaces.length; i++)
-    {
+    for (var i = 0; i < blankSpaces.length; i++) {
         var l = currentWord[i].toLowerCase();
         var letterLower = letter.toLowerCase();
-        if (l === letterLower)
-        {
+        if (l === letterLower) {
             blankSpaces[i] = currentWord[i];
         }
     }
 }
 
-    //bonus* play a sound when the user guesses the word correctly
+//bonus* play a sound when the user guesses the word correctly
 
 
-    //bonus* stylish CSS rules to make design that fits game theme
+//bonus* stylish CSS rules to make design that fits game theme
 
 
-    //bonus* organize game code as object, except for key events that get to the letter guessed
+//bonus* organize game code as object, except for key events that get to the letter guessed
