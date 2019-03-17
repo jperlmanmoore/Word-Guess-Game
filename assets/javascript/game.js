3792171c675f62072b18790e8f1f91745fe65af7
jperlmanmoore <jperlmanmoore@gmail.com>
2019-03-17 11:22:51 -0400

    some progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,66 @@
-//what are my parts - vars - global b/c outside of a function
 var wordList = ["Superwomen", "Supergirl", "Harley Quinn", "Lois Lane",
     "Power Girl", "Captain Marvel", "Catwoman", "Black Widow",
     "She-Hulk", "Elektra", "Mystique", "Hela", "Nebula",
     "The Femme Fatales", "Gamora", "Green Lantern", "Invisible Woman", "Thor"
 ];
-var blankSpaces = "_"
-var currentWord; //what word was random picked
-var lettersInWord; //what letters are in the current word
-var userGuess = []; //what letters the user has guessed
-var blankCorrect = []; //correct letters go here
-var wrongLetters = []; //wrong letters
-var wins = 0 //how many wins the player has
-var losses = 0 //how many losses the player has
-var guessesLeft = 10 //how many guesses the player has left
-var remainingLetters = userGuess //what letters have not been picked 
-var alphabetArray = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
+var currentWord = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(currentWord);
 
 //if game has not started, when a user has entered a letter, random word is picked and game starts
-
-//if current word is null start a game & pick a work, otherwise check guesses & only in that condition
-
-currentWord = wordList[Math.floor(Math.random() * wordList.length)]; //picks a random word from wordList
-lettersInWord = currentWord.split(""); //.split = split a string into substring - shows how many letters are in the currentWord
-blankSpaces = lettersInWord.length; //says how many blanks to make
-for (var i = 0; i < lettersInWord; i++) { //loop to determine how many letters
-    blankSpaces.push("_"); //creates blanks
-}
 guessesLeft = 10; //how many guesses at start
-wrongLetters = []; //how many wrong letters
+//document.getElementById("guessesLeft");
 
+var blankSpaces = [];
 
-
-console.log(blankSpaces)
-console.log(currentWord)
-console.log(lettersInWord)
-console.log(blankCorrect)
+function generateUnderscores() {
+    for (i = 0; i < currentWord.length; i++) {
+        blankSpaces.push("_");
+    }
+    return blankSpaces;
+}
+console.log(generateUnderscores());
+//document.getElementById("blankspaces");
 
 //guesses  - on event keyup checking guesses with event.key
-function checkGuesses(letter) { //you can name function anything you want
-    var lettersInWord = false; //see if letter is in the current word
-    for (var i = 0; i < blankSpaces; i++) { //if currentWord = letter entered, then :)
-        if (currentWord[i] == letter) {
-            lettersInWord = true;
-            guessesLeft--; //guess has been used
-        }
-    }
-    if (lettersInWord) { //does guessed letter match word?
-        for (var i = 0; i < blankSpaces; i++) {
-            if (currentWord[i] == letter) {
-                blankCorrect[i] = letter;
-            }
-        }
-console.log(blankCorrect);
-    } else { //if wrong letter guesses, then :(
-        wrongLetters.push(wrongletters); //put wrong letter in space wehre it should go ???
-        guessesLeft--; //guess has been used
-    }
-}
-console.log(wrongLetters);
-
-
-///get results in page! :)
-
-//startover if player wins or looses
-//function reset() {
-    //guessesLeft = 10;
-    //wrongLetters = [];
-    //blankSpaces = [];
-    //startGame();
-//}
-
-//start game - any key selected
-
 //document.onkeyup = function (event) {
-    //console.log(event.key);
+//var userGuess = event.key;
 //}
 
-function initializeGame () {
-    currentWord = "";
-    guessesLeft = 10;
-    wrongLetters = [];
-    blankSpaces = [];
+var correctLetter = [];
+var wrongLetter = [];
+
+//determine a guess
+document.addEventListener("keypress", (event) => {
+    var letter = String.fromCharCode(event.keyCode);
+    console.log(currentWord.indexOf(letter));
+
+    //put correct letter is guessed 
+    if (currentWord.indexOf(letter) > -1) {
+        //put in correct letter array
+        correctLetter.push(letter);
+        console.log(correctLetter);
+       
+        //replace underscore
+        blankSpaces[currentWord.indexOf(letter)] = letter;
+        if(blankSpaces.join("") === currentWord) {
+            document.getElementById("winorlose") = "You won!";
+        }
+    } else {
     
-    $("#currentWord, #guessesLeft, #wrongLetters, #blankSpaces").empty();
-  }
+    //put wrong letter in array
+    if (letter.indexOf(currentWord) < 0) {
+        wrongLetter.push(letter);
+        console.log(wrongLetter);
+        }
+    }
+    
+})
 
 //bonus* play a sound when the user guesses the word correctly
 
 
-//bonus* stylish CSS rules to make design that fits game theme
+    //bonus* stylish CSS rules to make design that fits game theme
 
 
-//bonus* organize game code as object, except for key events that get to the letter guessed
+    //bonus* organize game code as object, except for key events that get to the letter guessed
